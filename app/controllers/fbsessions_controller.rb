da1395208aa3377699592a24ab9ec74b01e7cf5e
Next <nextacademy@email.com>
2015-11-17 03:15:55 -0800

    initial commit for searchfeature

--- app/controllers/fbsessions_controller.rb
+++ app/controllers/fbsessions_controller.rb
@@ -4,7 +4,7 @@ class FbsessionsController < ApplicationController
 	  def create
 	    @user = User.from_omniauth(env["omniauth.auth"])
 	    session[:user_id] = @user.id
-	    redirect_to user_path(@user)
+	    redirect_to root_path
  		end
 
 	  def destroy
