8288575cbc5f991f854aff21d7c98f0fdfe731c1
Next <nextacademy@email.com>
2015-11-17 19:25:49 -0800

    fixed exclude date edges for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,6 @@ class Booking < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :listing
 	validates :checkin, :checkout, :user_id, :listing_id, presence: true
-	validates :checkin, :checkout, :overlap => {:scope => "listing_id"}
+	validates :checkin, :checkout, :overlap => {:exclude_edges => ["checkin", "checkout"], :scope => "listing_id"}
 end
 
